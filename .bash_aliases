f836b1e14b425a476d9c62f2bf9be2e7a7acb24a
Hamath-Eltrael <marvin@hamath.de>
2021-02-09 19:19:06 +0100

    Updated everything for New Laptop Main-Machine, started symlinking dotfiles instead of copying

--- .bash_aliases
+++
@@ -1,2 +0,0 @@
-alias backup-server-hdd='rsync -aAXv --info=progress2 hamath@192.168.1.101:/mnt/hdd /mnt/HDD/server-hdd'
-alias server='ssh hamath@192.168.1.101'
---
+++ .screenlayout/temp.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 3840x2160 --pos 0x1440 --rotate normal --dpi 175 --output DP-1-1.3 --mode 2560x1440 --pos 475x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --off
