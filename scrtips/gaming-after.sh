797e0ccf807ffb6cc00d78d9e5cdced9bb4bad5d
Hamath-Eltrael <marvin@hamath.de>
2021-05-24 09:49:29 +0200

    added scripts to perform pre and post gaming stuff quick and managed from one central place

---
+++ scrtips/gaming-after.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# activate tochpad
+xinput --enable "SynPS/2 Synaptics TouchPad"
+
+# set laptopscreen to 4k and the secondary monitor to WQHD
+xrandr --output eDP-1 --primary --mode 3840x2160 --pos 0x1440 --rotate normal --dpi 175 --output DP-1-1.3 --mode 2560x1440 --pos 475x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --off
+
+# set the dock-speakers as soundoutput
+pactl set-default-sink alsa_output.usb-Lenovo_ThinkPad_USB-C_Dock_Gen2_USB_Audio_000000000000-00.analog-stereo
---
+++ scrtips/gaming-before.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# deactivate the touchpad
+xinput --disable "SynPS/2 Synaptics TouchPad"
+
+# deactivate other screens and set laptop-screen to 1080p resolution
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --dpi 100 --output DP-1-1.3 --off --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --off
+
+# set the headphones as soundoutput
+pactl set-default-sink alsa_output.usb-ASUSTeK_Strix_7.1-00.analog-surround-71
