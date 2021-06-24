4bc1f725063ff1980359a0d10c0555752618fe40
Hamath-Eltrael <marvin@hamath.de>
2021-06-24 22:13:56 +0200

    added restart of i3 to the screenlayouts; just so it does it on its own when I change layout

--- .screenlayout/gaming.sh
+++ .screenlayout/gaming.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --dpi 100 --output DP-1-1.3 --off --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --off
+
+# restart i3 for bar-fitment
+i3-msg restart
--- .screenlayout/solo.sh
+++ .screenlayout/solo.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 xrandr --output eDP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --dpi 175 --output DP-1-1.3 --off --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --off
+
+# restart i3 for bar-fitment
+i3-msg restart
